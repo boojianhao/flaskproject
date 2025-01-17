78a1c133c842778f1145e057866b8810b02a931f
boojianhao <boojianhao@gmail.com>
2025-01-17 18:54:41 +0800

    Delete admin.py

--- admin.py
+++
@@ -1,9 +0,0 @@
-# admin.py
-from customer import Users
-
-class Admin(Users):
-    def __init__(self, email, password, name, security_question_1, security_question_2, active=True):
-        super().__init__(email, password, name, security_question_1, security_question_2, active)
-
-    def is_admin(self):
-        return True
